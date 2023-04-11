4dd916ce6bf8a418b6bac3a9235830d98708818d
Tim Konick <konick781@gmail.com>
2023-04-11 19:47:57 -0400

    Author log

--- stale-branches.sh
+++ stale-branches.sh
@@ -17,8 +17,7 @@ echo "Running for $ORG"
 
 WORKSPACE="Workspace"
 OUTPUT="$PWD/Stale-Branches.txt"
-# XXX Default to 180 days adjust as needed
-#     Set this value as needed
+# Default to 180 days adjust as needed
 PERIOD=180
 TARGET=$(date -v -"$PERIOD"d "+%Y-%m-%d")
 echo "Any branches without activity from $TARGET will be removed"
@@ -51,15 +50,19 @@ do
     fi
     echo "Project: " $repository >> $OUTPUT 
     cd $repository
-    # only list branches beyond 180 of inactivity
-    for branch in $(git for-each-ref --sort=-committerdate --format='%(committerdate:format:%Y-%m-%d %I:%M %p) %(refname)' | awk -v DATE=$TARGET '{ if ( index($4, "remotes") != 0 && $1 < DATE ) { print $1":"$4 } }')
+    # only list branches beyond PERIOD of inactivity
+    for branch in $(git for-each-ref --sort=-committerdate \
+                    --format='%(committerdate:format:%Y-%m-%d %I:%M %p) %(refname) %(authorname)'\
+                    | awk -v DATE=$TARGET '{ if ( index($4, "remotes") != 0 && $1 < DATE ) { print $1":"$4":"$5":"$6 } }')
     do
         timestamp=$(cut -d':' -f1 <<< $branch);
         branch_name=$(cut -d'/' -f4- <<< $branch);
+        branch_name=$(cut -d':' -f1 <<< $branch_name);
+        author=$(cut -d':' -f3- <<< $branch);
         if [[ $branch_name != "HEAD" && $branch_name != "main" && $branch_name != "master" && $branch_name != "develop" ]]
         then
-            echo "    Date: $timestamp Branch: $branch_name" >> $OUTPUT
-            # XXX uncomment to permanently delete branches from remote
+            echo "    Last-Commit: $timestamp Author: $author Branch: $branch_name" >> $OUTPUT
+            # XXX WARNING PERMANENTLY DELETES UPSTREAM REMOTE BRANCH
             #git push origin -d $branch_name
         fi
     done
